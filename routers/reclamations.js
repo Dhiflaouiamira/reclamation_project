3cfcd6a52699144f63008b5ffed1d0a8b39dcd98
Dhiflaouiamira <dhiflaouiamira98@gmail.com>
2023-11-06 12:13:45 +0100

    first commit

---
+++ routers/reclamations.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const Reclamation = require('../models/Reclamation');
+const User = require('../models/User'); // Import the User model
+const auth = require ('../middleware/auth');
+
+const router = express.Router();
+
+// GET all reclamations
+router.get('/', (req, res) => {
+    Reclamation.find()
+        .exec()
+        .then(reclamations => res.send(reclamations))
+        .catch(err => res.status(400).send());
+});
+
+// GET all reclamations
+router.get('/', (req, res) => {
+    Reclamation.find({userId: req.params.userId})
+        .exec()
+        .then(reclamations => res.send(reclamations))
+        .catch(err => res.status(400).send());
+});
+
+// GET all reclamations for a specific user
+router.get('/user/:userId', async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findById(userId).populate('reclamations');
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.send(user.reclamations);
+    } catch (error) {
+        console.error('Error fetching reclamations for the user:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// POST a reclamation for a specific user
+router.post('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        const reclamation = await Reclamation.create(req.body);
+        user.reclamations.push(reclamation);
+        await user.save();
+        res.send(reclamation);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    }
+});
+//POST
+router.post('/', (req, res) => {
+    const newReclamation =   new Reclamation(req.body);
+    newReclamation.save()
+      .then(reclamation => res.send(newReclamation))
+      .catch(err => res.status(400).send(err));
+  });
+
+
+  
+// GET a reclamation by ID
+router.get('/:id', (req, res) => {
+    Reclamation.findById(req.params.id)
+        .then(reclamation => {
+            if (!reclamation) {
+                res.status(404).send('Reclamation not found');
+            } else {
+                res.send(reclamation);
+            }
+        })
+        .catch(err => res.status(400).send(err));
+});
+
+// PUT update a reclamation
+router.put('/:id', (req, res) => {
+    Reclamation.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then(reclamation => {
+            if (!reclamation) {
+                res.status(404).json({ error: 1 });
+            } else {
+                res.send(reclamation);
+            }
+        })
+        .catch(err => res.status(400).send(err));
+});
+
+// DELETE a reclamation
+router.delete('/:id', (req, res) => {
+    Reclamation.findByIdAndDelete(req.params.id)
+        .exec()
+        .then(result => res.status(204).send())
+        .catch(err => res.status(404).json(err));
+});
+
+module.exports = router;
